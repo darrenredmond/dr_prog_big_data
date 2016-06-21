84ff4f2ee505f55fb895f4ca9f9a4e0fac6b2363
Darren Redmond <darren.redmond@gmail.com>
2016-06-21 15:31:16 +0100

    add the factorial python file

---
+++ factorial.py
@@ -0,0 +1,16 @@
+
+def factorial_simple(n):
+    if n == 0:
+       return 1
+    if n < 0:
+       return None
+    else:
+        return n * factorial_simple(n - 1)
+
+		
+def factorial(n):
+    return reduce(lambda x,y:x*y, [1]+range(1,n+1))
+
+if __name__ == '__main__':
+    value = int(raw_input('Enter a number?'))
+    print factorial_simple(value)
